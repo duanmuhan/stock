b42ffbc31937f879d60bae71c8ea8de767c1256e
Guangshu Cao <caoguangshu@tigerbrokers.com>
2017-06-15 19:12:59 +0800

    dao功能的添加

--- stock-trim/src/main/com/cgs/dao/KDao.java
+++ stock-trim/src/main/com/cgs/dao/KDao.java
@@ -1,5 +1,7 @@
 package com.cgs.dao;
 
+import com.cgs.entity.constant.GraphsRedisKeys;
+import com.cgs.entity.graphs.KEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
@@ -9,4 +11,9 @@ public class KDao {
 
   @Autowired
   private RedisTemplate<String,String> redisTemplate;
+
+  public void add(KEntity entity){
+    redisTemplate.opsForList().leftPush(GraphsRedisKeys.kDataKey(entity),entity.toRedisValue());
+  }
+
 }
