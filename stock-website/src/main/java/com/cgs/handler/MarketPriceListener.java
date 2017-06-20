a452b8ddc9b7ebcc98248ae0ce5b4fed61c8726a
Guangshu Cao <caoguangshu@tigerbrokers.com>
2017-06-20 19:48:01 +0800

    类名称的修改

---
+++ stock-website/src/main/java/com/cgs/handler/MarketPriceListener.java
@@ -0,0 +1,11 @@
+package com.cgs.handler;
+
+import javax.jms.MessageListener;
+
+public class MarketPriceListener implements MessageListener {
+
+  @Override
+  public void onMessage(javax.jms.Message message) {
+
+  }
+}
