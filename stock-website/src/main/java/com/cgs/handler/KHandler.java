6abcb4182ce838ee4989dd7c18a495fe187ffa89
Guangshu Cao <caoguangshu@tigerbrokers.com>
2017-06-20 16:46:34 +0800

    增加消息总线的监听者

---
+++ stock-website/src/main/java/com/cgs/handler/KHandler.java
@@ -0,0 +1,13 @@
+package com.cgs.handler;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
+
+public class KHandler implements MessageHandler {
+
+  @Override
+  public void handleMessage(Message<?> message) throws MessagingException {
+
+  }
+}
