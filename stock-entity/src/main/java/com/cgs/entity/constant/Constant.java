73b1917faa005f498ec54d9409e56b4a1373c5ce
Guangshu Cao <caoguangshu@tigerbrokers.com>
2017-06-20 15:29:00 +0800

    增加时间期限。

--- stock-entity/src/main/java/com/cgs/entity/constant/Constant.java
+++ stock-entity/src/main/java/com/cgs/entity/constant/Constant.java
@@ -1,4 +1,10 @@
 package com.cgs.entity.constant;
 
-public class Constant {
+public interface Constant {
+  long MIN_PERIOD = 60 * 1000;
+  long FIVE_MIN_PERIOD = 5 * 60 * 1000;
+  long FIFTEEN_MIN_PERIOD = 15 * 60 * 1000;
+  long HALF_HOUR_PERIOD = 30 * 60 * 1000;
+  long HOUR_PERIOD = 60 * 60 * 1000;
+  long DAY_PERIOD = 24 * 60 * 60 * 1000;
 }
