0e316200241f060a7087df5c43a6e4bcda40d4cc
Guangshu Cao <caoguangshu@tigerbrokers.com>
2017-06-20 17:12:20 +0800

    增加消息总线的监听者

---
+++ stock-trim/src/main/java/com/cgs/dao/MarketPriceDao.java
@@ -0,0 +1,18 @@
+package com.cgs.dao;
+
+import com.cgs.entity.constant.redis.GraphsRedisKeys;
+import com.cgs.entity.graphs.MarketPrice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MarketPriceDao {
+
+  @Autowired
+  private RedisTemplate<String,String> redisTemplate;
+
+  public void add(MarketPrice marketPrice){
+    redisTemplate.opsForList().leftPush(GraphsRedisKeys.marketValueKey(marketPrice),marketPrice.toRedisValue());
+  }
+}
