e0bccc104a4bd7323e6d6e8409690190e57e6fb5
duanmuhan <809653811@qq.com>
2017-07-02 21:59:47 +0800

    增加handler

--- stock-website/src/main/java/com/cgs/websocket/QuotesHandler.java
+++ stock-website/src/main/java/com/cgs/websocket/QuotesHandler.java
@@ -1,12 +1,17 @@
 package com.cgs.websocket;
 
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.BinaryWebSocketHandler;
 
 @Component
+@RequestMapping("/quotesHandler")
 public class QuotesHandler extends BinaryWebSocketHandler {
+
+    private SimpMessagingTemplate template;
 
     @Override
     protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
--- stock-website/src/main/java/com/cgs/websocket/TechHandler.java
+++ stock-website/src/main/java/com/cgs/websocket/TechHandler.java
@@ -1,14 +1,40 @@
 package com.cgs.websocket;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.handler.BinaryWebSocketHandler;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
-public class TechHandler extends BinaryWebSocketHandler {
+@RequestMapping("/techHandler")
+public class TechHandler extends TextWebSocketHandler {
+
+    private Map<String,WebSocketSession> sessionMap = new ConcurrentHashMap<>();
 
     @Override
-    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        if (session.isOpen()){
+            System.out.println("fuckyou");
+        }
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessionMap.put(session.getId(),session);
+        System.out.println("fuckyou established" + session.getId());
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        System.out.println("closed");
+    }
+
+    public Map<String,WebSocketSession> getSessionMap(){
+        return sessionMap;
     }
 }
